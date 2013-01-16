56bd8c18b92c09094aa14ef874dd3cf6e79dcfba
Mikola Lysenko <mikolalysenko@gmail.com>
2013-01-16 12:40:18 -0600

    Adding files split from trimesh

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 mesh-topology
 =============
 
-Topological operations and indexing for meshes
+Topological operations and indexing for simplicial complexes (ie triangular meshes, tetrahedral meshes, etc.)
+
+
+`skeleton(cells, n)`
+--------------------
+Finds the n-skeleton of a simplicial complex
+
+`buildIndex(cells, n_skel, n)`
+------------------------------
+Builds an index for neighborhood queries
+
+`normalize(cells)`
+------------------
+Canonicalizes a cell complex.  Sorts all cells and removes duplicates.  Does not preserve orientation.
+
+`findCell(cells, c, sorted)`
+-----------------------------
+Finds the cell `c` in the normalized array of cells.
+
+`cloneCells(cells)`
+-------------------
+Makes a copy of a cell complex
+
+`countVertices(cells)`
+----------------------
+Counts the number of vertices.  Short hand for:
+
+    skeleton(cells, 1).length
+
+
+`stars(cells[, vertex_count])`
+------------------------------
+A more optimized way to build an index for vertices.  Formally equivalent to the following:
+
+    buildIndex(cells, skeleton(cells, 1), 1);
+
+Credits
+=======
+
+
---
+++ topology.js
@@ -0,0 +1,148 @@
+"use strict"; "use restrict";
+
+var bits = require("bit-twiddle");
+
+//Counts the number of vertices in faces
+function countVertices(cells) {
+  var vc = 0;
+  for(var i=0; i<cells.length; ++i) {
+    var c = cells[i];
+    for(var j=0; j<c.length; ++j) {
+      vc = Math.max(vc, f[j]);
+    }
+  }
+  return vc+1;
+}
+exports.countVertices = countVertices;
+
+//Clone cells
+function cloneCells(cells) {
+  var ncells = new Array(cells.length);
+  for(var i=0; i<cells.length; ++i) {
+    ncells[i] = cells[i].slice(0);
+  }
+  return ncells;
+}
+exports.cloneCells = cloneCells;
+
+//Computes the vertex stars for the mesh
+function stars(cells, vertex_count) {
+  if(!vertex_count) {
+    vertex_count = countVertices(faces);
+  }
+  var stars = new Array(vertex_count);
+  for(var i=0; i<vertex_count; ++i) {
+    stars[i] = [];
+  }
+  for(var i=0; i<cells.length; ++i) {
+    var c = cells[i];
+    for(var j=0; j<c.length; ++j) {
+      stars[c[j]].push(i);
+    }
+  }
+  return stars;
+};
+exports.stars = stars;
+
+var lexCompare = new Function("a", "b", [
+  "for(var i=Math.min(a.length,b.length)-1; i>=0; --i) {",
+    "var d = a[i] - b[i];",
+    "if(d) { return d; }",
+  "}",
+  "return a.length - b.length;"
+].join("\n"));
+
+//Sort cells, break orientation but facilitates indexing
+function normalize(cells) {
+  //Sort cells first
+  for(var i=0; i<cells.length; ++i) {
+    cells[i].sort();
+  }
+  cells.sort(lexCompare);
+  //Remove duplicates
+  var ptr = 1;
+  for(var i=1; i<cells.length; ++i) {
+    if(lexCompare(cells[i], cells[i-1])) {
+      var a = cells[i]
+        , b = cells[ptr++];
+      for(var j=0; j<n; ++j) {
+        b[j] = a[j];
+      }
+    }
+  }
+  cells.length = ptr;
+  return cells;
+}
+exports.normalize = normalize;
+
+//Computes the n-skeleton of the cell complex
+function skeleton(cells, n) {
+  var skel = [];
+  for(var i=0; i<cells.length; ++i) {
+    var c = cells[i];
+    for(var k=(1<<(n+1))-1; k<(1<<c.length); k=bits.nextCombination(k)) {
+      var b = new Array(n)
+        , l = 0;
+      for(var j=0; j<c.length; ++j) {
+        if(k & (1<<j)) {
+          b[l++] = c[j];
+        }
+      }
+      skel.push(b);
+    }
+  }
+  return normalize(skel)
+}
+exports.skeleton = skeleton;
+
+//Finds an ordered cell
+function findCell(cells, c, sorted) {
+  if(!sorted) {
+    c = c.slice(0);
+    c.sort();
+  }
+  var lo = 0
+    , hi = cells.length-1;
+  while (lo <= hi) {
+    var mid = (lo + hi) >> 1;
+      , s   = lexCompare(cells[mid], c);
+    if(s < 0) {
+      lo = mid + 1;
+    } else if(s > 0) {
+      hi = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+exports.findCell = findCell;
+
+//Builds an index for an n-cell.  This is more general than stars, but less efficient
+function buildIndex(cells, n_skel, n) {
+  var index = new Array(n_skel.length);
+  for(var i=0; i<index.length; ++i) {
+    index[i] = [];
+  }
+  for(var i=0; i<cells.length; ++i) {
+    var c = cells[i].slice(0);
+    c.sort();
+    for(var k=(1<<(n+1))-1; k<(1<<c.length); k=bits.nextCombination(k)) {
+      var b = new Array(n)
+        , l = 0;
+      for(var j=0; j<c.length; ++j) {
+        if(k & (1<<j)) {
+          b[l++] = c[j];
+        }
+      }
+      var idx = findCell(n_skel, b, true);
+      if(idx >= 0) {
+        index[idx].push(i);
+      }
+    }
+  }
+  return idx;
+}
+exports.buildIndex = buildIndex;
+
+
